869c9676e861a6df2326dc68ff4f85c01b1157c5
Daulet Tumbayev <daulettbot@gmail.com>
2024-04-20 21:30:15 +0500

    intial task files

---
+++ Q1.lua
@@ -0,0 +1,14 @@
+-- @author: Daulet Tumbayev
+-- @date: 04/20/2024
+-- Q1 - Fix or improve the implementation of the below methods
+
+local function releaseStorage(player)
+    player:setStorageValue(1000, -1)
+end
+
+function onLogout(player)
+    if player:getStorageValue(1000) == 1 then
+        addEvent(releaseStorage, 1000, player)
+    end
+    return true
+end
